7e47ad034f111b7aaf54b24c2f5a6559a2bd1531
Tiago Silva <tiago.silva@email.com>
2020-09-11 19:28:54 -0300

    Update 001-MainProcess.py
    Excluded all path and names from code

--- 001-MainProcess.py
+++ 001-MainProcess.py
@@ -14,10 +14,10 @@ from pandas.tseries.offsets import BDay
 from ftplib import FTP
 
 #Server Path
-serverPath="//ZEUS/CONTROLES_INTERNOS_BI$/"
+serverPath="File_Server_Path" # incluir o caminho do servidor de arquivos
 
 #Setup WD
-WorkDirectory = "19-DataManagement/002-Projetos/012-FileManagement/"
+WorkDirectory = "WorkingDirectory" # incluir o diretório onde o projeto será salvo
 os.chdir(serverPath+WorkDirectory)
 os.getcwd()
 
@@ -29,15 +29,14 @@ procDate = pd.datetime.today() - BDay(1)
 procDate = procDate.strftime('%Y%m%d')
 
 # ================================================================================================
-# Suitability Files
+# Files from directory IT
 # ================================================================================================
 
-fromPath=serverPath+"Arquivos TI/"
-toPath=serverPath+"19-DataManagement/000-ArquivosFonte/00-Suitability/"
-listFiles=["Nota_Risco.txt","PERF-INV.TXT","SUIT_REL_CLIE.TXT",
-              "SUIT_REL_CLIE_DECLARACAO.TXT","SUIT_TERMOS_PLAN.TXT"]
+fromPath=serverPath+"Arquivos_TI/"
+toPath=serverPath+"Caminho_onde_será salvo os arquivos baixados"
+listFiles=["lista de arquivos ou formato de arquivos a serem baixados"]
 
-print("--- Copying Suitability files ---")
+print("--- Copying files ---")
 startTime = time.time()
 CopyFiles(fromPath,toPath,listFiles)
 RenameFiles(toPath,listFiles)
@@ -46,22 +45,22 @@ endTime = time.time()
 timer(startTime,endTime)
 with open(serverPath+WorkDirectory+'log.txt', 'a') as logFile:    
     logFile.write("\nData Processamento: " + pd.datetime.today().strftime('%d/%m/%Y %H:%M:%S') + \
-                  " --> Suitability " + timer(startTime,endTime))
+                  " --> Nome Arquivo " + timer(startTime,endTime))
 
 # ================================================================================================
-# GCA Files
+# GCA Files FTP Server files 
 # ================================================================================================
-toPath=serverPath+"19-DataManagement/000-ArquivosFonte/01-GCA/"
-fileName = open(toPath+procDate+"_GCA.TXT",'wb')
+toPath=serverPath+"Onde salvar os arquivos"
+fileName = open(toPath+procDate+"_Files.TXT",'wb')
 
-print("--- Copying GCA files ---")
+print("--- Copying files ---")
 startTime = time.time()
 # FTP Config
-ftp = FTP('172.16.8.250')
-ftp.login(user='tiagcon',passwd='ts201912')
+ftp = FTP('serverIP')
+ftp.login(user='Usuário',passwd='Senha')
 
 # Download File
-ftp.retrbinary("RETR 'GCA.PS312.SEXPB532(0)'", fileName.write ,1024)
+ftp.retrbinary("RETR 'Nome arquivo servidor FTP'", fileName.write ,1024)
 ftp.quit()
 fileName.close()
 ZipFiles(toPath, procDate)
@@ -70,4 +69,4 @@ endTime = time.time()
 timer(startTime,endTime)
 with open(serverPath+WorkDirectory+'log.txt', 'a') as logFile:    
     logFile.write("\nData Processamento: " + pd.datetime.today().strftime('%d/%m/%Y %H:%M:%S') + \
-                  " --> GCA " + timer(startTime,endTime))
+                  " --> FTP File " + timer(startTime,endTime))
