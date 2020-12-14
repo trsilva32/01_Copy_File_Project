466bc0346390f56e7c7dbcc46493a9a26c68d107
Tiago Silva <tiago.silva@email.com>
2020-12-14 18:59:13 -0300

    Initial Commit

--- 001-MainProcess.py
+++ 001-MainProcess.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-VS Code - Project
-Projeto: Copiar 
-
-Este é um arquivo de script temporário.
-"""
-
 #Import Libraries
 import os
 import time
@@ -14,59 +6,59 @@ from pandas.tseries.offsets import BDay
 from ftplib import FTP
 
 #Server Path
-serverPath="File_Server_Path" # incluir o caminho do servidor de arquivos
+serverPath="File_Server_Path/" # path to collect files from
 
 #Setup WD
-WorkDirectory = "WorkingDirectory" # incluir o diretório onde o projeto será salvo
+WorkDirectory = "WorkingDirectory/" # path of this file is saved
 os.chdir(serverPath+WorkDirectory)
 os.getcwd()
 
 #Import Functions
-from Functions import *
+from Functions import * # Import Functions from the functions.py 
 
 #Global Variables
-procDate = pd.datetime.today() - BDay(1) 
+procDate = pd.datetime.today() - BDay(1) # Get last Business day Date
 procDate = procDate.strftime('%Y%m%d')
 
 # ================================================================================================
-# Files from directory IT
+# Files from Network Folder ---> Copy it to your processing folder 
 # ================================================================================================
 
-fromPath=serverPath+"Arquivos_TI/"
-toPath=serverPath+"Caminho_onde_será salvo os arquivos baixados"
-listFiles=["lista de arquivos ou formato de arquivos a serem baixados"]
+fromPath=serverPath+"From_Folder/"
+toPath=serverPath+"To_Folder"
+listFiles=["List of files to be copied from the From_Folder"]
 
-print("--- Copying files ---")
-startTime = time.time()
+print("--- Copying files ---> ", fromPath,' to ', toPath ) 
+startTime = time.time() # variable to controles time spent copying files
 CopyFiles(fromPath,toPath,listFiles)
 RenameFiles(toPath,listFiles)
-ZipFiles(toPath, procDate)
-endTime = time.time()
-timer(startTime,endTime)
-with open(serverPath+WorkDirectory+'log.txt', 'a') as logFile:    
-    logFile.write("\nData Processamento: " + pd.datetime.today().strftime('%d/%m/%Y %H:%M:%S') + \
-                  " --> Nome Arquivo " + timer(startTime,endTime))
+ZipFiles(toPath, procDate) # Save file on Processing Date Folder
+endTime = time.time() # variable to controles time spent copying files
+timer(startTime,endTime) # Spent time
+with open(serverPath+WorkDirectory+'log.txt', 'a') as logFile: # Log file   
+    logFile.write("\nProcessing Date: " + pd.datetime.today().strftime('%d/%m/%Y %H:%M:%S') + \
+                  " --> File Name " + timer(startTime,endTime))
 
 # ================================================================================================
-# GCA Files FTP Server files 
+# Files FTP Server 
 # ================================================================================================
-toPath=serverPath+"Onde salvar os arquivos"
-fileName = open(toPath+procDate+"_Files.TXT",'wb')
+toPath=serverPath+"To_Folder"
+fileName = open(toPath+procDate+"_Files.TXT",'wb') # File New Name
 
-print("--- Copying files ---")
-startTime = time.time()
+print("--- Downloading FTP File ---")
+startTime = time.time() # variable to controles time spent copying files
 # FTP Config
 ftp = FTP('serverIP')
 ftp.login(user='Usuário',passwd='Senha')
 
 # Download File
-ftp.retrbinary("RETR 'Nome arquivo servidor FTP'", fileName.write ,1024)
+ftp.retrbinary("RETR 'File_Name_FTP_Server'", fileName.write ,1024)
 ftp.quit()
 fileName.close()
-ZipFiles(toPath, procDate)
+ZipFiles(toPath, procDate) # Save file on Processing Date Folder
 
-endTime = time.time()
-timer(startTime,endTime)
-with open(serverPath+WorkDirectory+'log.txt', 'a') as logFile:    
+endTime = time.time() # variable to controles time spent copying files
+timer(startTime,endTime) # Spent time
+with open(serverPath+WorkDirectory+'log.txt', 'a') as logFile: # Log File
     logFile.write("\nData Processamento: " + pd.datetime.today().strftime('%d/%m/%Y %H:%M:%S') + \
                   " --> FTP File " + timer(startTime,endTime))
